7b52ac2b8c37e0d247f4832469d6c4363aeff35c
Vignesh Viswanathan <viswanat@codeaurora.org>
2017-11-21 16:21:34 +0530

    wlan: Add sanity check to limit mgmt frames data len
    Currently the frameLen in Rx pkt meta is not checked for
    upper bound in peHandleMgmtFrame.
    
    Add sanity check to drop the packet if frameLen is
    greater than 2000 bytes. Also add upper bound check for
    frameLen in limProcessAuthFrame function.
    
    SQK: backported for old driver
    
    Change-Id: Id8d80f892c18d044896224c22b21f667ee30eb6b
    CRs-Fixed: 2146331

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAuthFrame.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessAuthFrame.c
@@ -253,7 +253,7 @@ limProcessAuthFrame(tpAniSirGlobal pMac, tANI_U8 *pRxPacketInfo, tpPESession pse
             return;
         }
 
-        if (frameLen < LIM_ENCR_AUTH_BODY_LEN)
+        if (frameLen != LIM_ENCR_AUTH_BODY_LEN)
         {
             // Log error
             limLog(pMac, LOGE,
