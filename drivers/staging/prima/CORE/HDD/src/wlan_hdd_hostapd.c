dd1f62cbf391d812eb30806371b1e25b35710ece
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2018-01-02 19:52:26 +0530

    wlan: Bounds check key length in __iw_set_ap_encodeext
    propagation from qcacld-3.0 to prima
    
    ___iw_set_ap_encodeext iterates over a key buffer based on a length that
    may be larger than the key buffer size. Bounds check the key length
    before iterating over the buffer, and reject the call if the length is
    too large.
    
    Change-Id: I2473905500470ab6c4e83f5822949bbc9d94f5f1
    CRs-Fixed: 2165804

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_hostapd.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_hostapd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -3304,7 +3304,7 @@ static int __iw_set_ap_encodeext(struct net_device *dev,
          /*Convert from 1-based to 0-based keying*/
         key_index--;
     }
-    if(!ext->key_len) {
+    if(!ext->key_len || ext->key_len > CSR_MAX_KEY_LEN) {
 #if 0
       /*Set the encrytion type to NONE*/
 #if 0
@@ -3350,7 +3350,7 @@ static int __iw_set_ap_encodeext(struct net_device *dev,
              retval = -EINVAL;
          }
 #endif
-         return retval;
+         return -EINVAL;
 
     }
     
@@ -3359,9 +3359,7 @@ static int __iw_set_ap_encodeext(struct net_device *dev,
     setKey.keyId = key_index;
     setKey.keyLength = ext->key_len;
    
-    if(ext->key_len <= CSR_MAX_KEY_LEN) {
-       vos_mem_copy(&setKey.Key[0],ext->key,ext->key_len);
-    }   
+    vos_mem_copy(&setKey.Key[0],ext->key,ext->key_len);
    
     if(ext->ext_flags & IW_ENCODE_EXT_GROUP_KEY) {
       /*Key direction for group is RX only*/
